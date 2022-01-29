0b59bc6db080b96a32e205a5761246761415bb21
franci301 <gregotti20@gmail.com>
2022-01-29 11:56:26 +0000

    added date button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 function updateTimer() {
-
     future  = Date.parse("February 28 2022 13:30");
     now     = new Date();
     diff    = future - now;
@@ -21,20 +20,50 @@ function updateTimer() {
         '<div> ' + m + ' <span>minutes</span></div> ' +
         '<div> ' + s + ' <span>seconds</span></div> ' ;
   }
-  setInterval('updateTimer()', 1000 );
+var firstInterval = setInterval('updateTimer()', 1000 );
   
+function changeDate(){
+    var newDate = document.getElementById("dateChange").value
+    future  = Date.parse(newDate);
+    now     = new Date();
+    diff    = future - now;
+
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+    
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+    
+    document.getElementById("timer")
+        .innerHTML =
+        '<div> ' + d + ' <span>days</span></div> ' +
+        '<div> ' + h + ' <span>hours</span></div> ' +
+        '<div> ' + m + ' <span>minutes</span></div> ' +
+        '<div> ' + s + ' <span>seconds</span></div> ' ;
+}
+
+function change(){
+    clearInterval(firstInterval);
+    changeDate();
+    setInterval('change()', 1000 )
+}
+
 if(document.cookie != ''){
     let decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie);
     let ca = decodedCookie.split(';');
     ca = ca[0].split('=')[1];
     document.getElementById("text").innerHTML = "";
     document.getElementById("text").innerHTML = "Countdown until " + ca;
+    
 }else{
     document.getElementById("text").innerHTML = "";
     document.getElementById("text").innerHTML = "Countdown until ..." ;
 }
-
-console.log(document.cookie);
 
 function changeText(){
     var text = document.getElementById("getText").value;
@@ -52,5 +81,4 @@ function changeText(){
             document.getElementById("text").innerHTML = "Countdown until " + text;
         }
     }
-    
 }
