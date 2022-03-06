ed899b48f3563ff7e6538b20d77cecf7765a87d8
franci301 <52578792+franci301@users.noreply.github.com>
2022-03-06 20:23:40 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,90 +1,63 @@
 function updateTimer() {
-    future  = Date.parse("April 18 2022 10:00");
-    now     = new Date();
-    diff    = future - now;
 
-    days  = Math.floor( diff / (1000*60*60*24) );
-    hours = Math.floor( diff / (1000*60*60) );
-    mins  = Math.floor( diff / (1000*60) );
-    secs  = Math.floor( diff / 1000 );
-  
-    d = days;
-    h = hours - days  * 24;
-    m = mins  - hours * 60;
-    s = secs  - mins  * 60;
-  
-    document.getElementById("timer")
-      .innerHTML =
-        '<div> ' + d + ' <span>days</span></div> ' +
-        '<div> ' + h + ' <span>hours</span></div> ' +
-        '<div> ' + m + ' <span>minutes</span></div> ' +
-        '<div> ' + s + ' <span>seconds</span></div> ' ;
-  }
-var firstInterval = setInterval('updateTimer()', 1000 );
-  
-function changeDate(newDate){
-    future  = Date.parse(newDate);
-    now     = new Date();
-    diff    = future - now;
+    future = Date.parse("February 28 2022 13:30");
+    now = new Date();
+    diff = future - now;
 
-    days  = Math.floor( diff / (1000*60*60*24) );
-    hours = Math.floor( diff / (1000*60*60) );
-    mins  = Math.floor( diff / (1000*60) );
-    secs  = Math.floor( diff / 1000 );
-    
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
     d = days;
-    h = hours - days  * 24;
-    m = mins  - hours * 60;
-    s = secs  - mins  * 60;
-    
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
+
     document.getElementById("timer")
         .innerHTML =
         '<div> ' + d + ' <span>days</span></div> ' +
         '<div> ' + h + ' <span>hours</span></div> ' +
         '<div> ' + m + ' <span>minutes</span></div> ' +
-        '<div> ' + s + ' <span>seconds</span></div> ' ;
-    
+        '<div> ' + s + ' <span>seconds</span></div> ';
 }
+setInterval('updateTimer()', 1000);
 
-function change(){
-    var newDate = document.getElementById("dateChange").value
-    if(newDate == ''){
-        document.getElementById("text").innerHTML = "";
-        document.getElementById("text").innerHTML = "Enter Something";
-    }else{
-        clearInterval(firstInterval);
-        changeDate(newDate);
-        setInterval('change()', 1000 )
-    }
-    
-}
-
-if(document.cookie != ''){
+if (document.cookie != '') {
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
     ca = ca[0].split('=')[1];
     document.getElementById("text").innerHTML = "";
     document.getElementById("text").innerHTML = "Countdown until " + ca;
-    
-}else{
+} else {
     document.getElementById("text").innerHTML = "";
-    document.getElementById("text").innerHTML = "Countdown until ..." ;
+    document.getElementById("text").innerHTML = "Countdown until ...";
 }
 
-function changeText(){
-    var text = document.getElementById("getText").value;
-    if(text==''){
-        document.getElementById("text").innerHTML = "";
-        document.getElementById("text").innerHTML = "Enter Something x";
-    }else{
-        if(document.cookies == null){
-            document.cookie = 'text='+text+'; expires=Tue, 18 April 2022 12:00:00 GMT';
-            document.getElementById("text").innerHTML = "";
-            document.getElementById("text").innerHTML = "Countdown until " + text;
-        }else{
-            document.cookie = 'text='+text+'; expires=Tue, 18 April 2022 12:00:00 GMT';
-            document.getElementById("text").innerHTML = "";
-            document.getElementById("text").innerHTML = "Countdown until " + text;
+console.log(document.cookie);
+
+function changeText() {
+    var input = document.getElementById("getText");
+    var text = input.value;
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            if (text == '') {
+                document.getElementById("text").innerHTML = "";
+                document.getElementById("text").innerHTML = "Enter Something x";
+            } else {
+                if (document.cookies == null) {
+                    document.cookie = 'text=' + text + '; expires=Tue, 1 Mar 2022 12:00:00 GMT';
+                    document.getElementById("text").innerHTML = "";
+                    document.getElementById("text").innerHTML = "Countdown until " + text;
+                } else {
+                    document.cookie = 'text=' + text + '; expires=Tue, 1 Mar 2022 12:00:00 GMT';
+                    document.getElementById("text").innerHTML = "";
+                    document.getElementById("text").innerHTML = "Countdown until " + text;
+                }
+            }
+
         }
-    }
+        event.preventDefault()
+        document.getElementById("btn").click()
+    })
 }
